3813c5d8cc6973b1694bf79079b6c6628d61d819
Grant Bigelow <grantbigelow23@gmail.com>
2018-04-11 12:06:48 -0500

    you know

---
+++ Cell.java
@@ -0,0 +1,111 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class Cell {
+	int     centerX;
+	int     centerY;
+	int     radius;
+	
+	Type    cellType;
+	Size    cellSize;
+	int     numOfTroops;
+	int     maxTroops;
+	double  regenRate; // troops per second
+	
+	int msDelay;
+	
+	public Cell(int centerX, int centerY, Type cellType, Size cellSize, int numOfTroops) {
+		this.centerX = centerX;
+		this.centerY = centerY;
+		this.cellType = cellType;
+		this.cellSize = cellSize;
+		this.numOfTroops = numOfTroops;
+		switch(cellSize){
+			case SMALL:
+				this.maxTroops = 15;
+				this.regenRate = 1;
+				this.radius    = 21;
+				break;
+			case MEDIUM:
+				this.maxTroops = 40;
+				this.regenRate = 1.2;
+				this.radius    = 36;
+				break;
+			case LARGE:
+				this.maxTroops = 100;
+				this.regenRate = 1.5;
+				this.radius    = 52;
+				break;
+		}
+	}
+	
+	public enum Type{
+		PLAYER, ENEMY, NEUTRAL
+	}
+	
+	public enum Size{
+		SMALL, MEDIUM, LARGE
+	}
+	
+	public void draw(Graphics g) {
+		switch(this.cellType) {
+			case PLAYER:
+				g.setColor(Color.BLUE);
+				break;
+			
+			case ENEMY:
+				g.setColor(Color.RED);
+				break;
+			case NEUTRAL:
+				g.setColor(Color.GRAY);
+				break;
+		}		
+		DrawObjects.drawCenteredCircle(g, this.centerX, this.centerY, this.radius);
+		g.setColor(Color.BLACK);
+		DrawObjects.drawNumber(g, this.numOfTroops, this.centerX, this.centerY, 18);
+	}
+	
+	public int getX() {
+		return this.centerX;
+	}
+	public int getY() {
+		return this.centerY;
+	}
+	
+	public Type getType() {
+		return this.cellType;
+	}
+	
+	public void addTroops(int numOfTroops, Type troopType) {
+		if (this.cellType == troopType) {
+			this.numOfTroops += numOfTroops;
+		}else if (numOfTroops <= this.numOfTroops) {
+			this.numOfTroops -= numOfTroops;
+		}else {
+			this.numOfTroops = numOfTroops - this.numOfTroops;
+			this.cellType = troopType;
+		}
+	}
+	
+	public void sendTroops(ArrayList<Troop> t, Cell otherCell) {
+		if (this.numOfTroops > 1) {
+			t.add(new Troop(this, otherCell, this.numOfTroops / 2));
+			this.numOfTroops /= 2;
+		} else if (this.numOfTroops == 1) {
+			t.add(new Troop(this, otherCell, this.numOfTroops / 2));
+			this.numOfTroops = 0;
+		}
+		
+	}
+	
+	public boolean isCoordInCell(int x, int y) {
+		if (Math.abs(this.centerX - x) <= this.radius || Math.abs(this.centerY - y) <= this.radius) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+}
---
+++ Troop.java
@@ -0,0 +1,82 @@
+import java.awt.Graphics;
+
+public class Troop {
+	double currentX;
+	double currentY;
+	int endX;
+	int endY;
+	
+	int numOfTroops;
+	
+	double distPerMove;
+	
+	Cell targetCell;
+	
+	Cell.Type troopType;
+	
+	boolean finishedMoving;
+	
+	double pxPerSec;
+	
+	public Troop(Cell cell1, Cell cell2, int numOfTroops) {
+		this.currentX    = cell1.getX();
+		this.currentY    = cell1.getY();
+		this.endX        = cell2.getX();
+		this.endY        = cell2.getY();
+		
+		this.numOfTroops = numOfTroops;
+		this.troopType   = cell1.getType();
+		
+		this.pxPerSec    = 15.0; 
+		
+		this.finishedMoving = false;
+		
+		
+	}
+	
+	public void move(int msDelta) {
+		double[] delta;
+		double distToMove = (msDelta / 1000.0) * this.pxPerSec;
+		if (!this.finishedMoving) {
+			if (getDistance(this.currentX, this.currentY, this.endX, this.endY) < distToMove) {
+				targetCell.addTroops(this.numOfTroops, this.troopType);
+				
+				this.finishedMoving = true;
+			} else {
+				delta = getXYDelta(getDistance(this.currentX, this.currentY, this.endX, this.endY),
+						           getSlope   (this.currentX, this.currentY, this.endX, this.endY));
+				this.currentX -= delta[0];
+				this.currentY -= delta[1];
+				System.out.println(""+delta[0]+""+delta[1]+" "+distToMove);
+			}
+		}
+		
+	}
+	
+	public void draw(Graphics g) {
+		if (!this.finishedMoving) {
+			DrawObjects.drawNumber(g, this.numOfTroops,(int) this.currentX, (int)this.currentY, 12);
+		}
+	}
+	
+	private double[] getXYDelta(double distance, double slope){
+		
+		double xDelta = Math.sqrt( Math.pow(distance, 2) / (Math.pow(slope, 2) + 1) );
+		double yDelta = xDelta * slope;
+		double[] ret = {(int) Math.round(xDelta),(int) Math.round(yDelta)};
+		return ret;
+	}
+	
+	private double getDistance(double x1,double y1,double x2,double y2) {
+		return Math.sqrt(Math.pow(x1 + x2, 2) + Math.pow(y1 + y2, 2));
+	}
+	
+	private double getSlope(double x1,double y1,double x2,double y2) {
+		return (y2 - y1) / (x2 - x1);
+	}
+	
+	private double getDelta(double a, double b) {
+		return b - a;
+	}
+
+}
